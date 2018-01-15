1ee031f43b8c75f65d72270919b69c56c8083a92
Unknown <quentin.darrigade@gmail.com>
2018-01-15 14:39:23 +0100

    Model Tetrimino

---
+++ ModelTetrimino.java
@@ -0,0 +1,44 @@
+package model;
+
+public class ModelTetrimino {
+	
+	private int id;
+	private String nom;
+	private String couleur;
+	
+	public void ModelTetrimino() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+
+	
+	public String toString() {
+		return "ModelTetrimino [id=" + id + ", nom=" + nom + ", couleur=" + couleur + "]";
+	}
+	
+	
+
+}
