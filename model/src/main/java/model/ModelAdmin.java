a5b7b74decf6eac55637a71399da90b5f14feeb9
Unknown <tommybrias@hotmail.com>
2018-01-19 10:33:00 +0100

    Merge branch 'DevTommy' into dev

--- model/src/main/java/model/ModelAdmin.java
+++ model/src/main/java/model/ModelAdmin.java
@@ -1,5 +1,17 @@
 package model;
 
-public class ModelAdmin {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "admin")
+@PrimaryKeyJoinColumn(name = "ADM_ID", referencedColumnName = "UTI_ID")
+public class ModelAdmin extends ModelUtilisateur {
+
+	@OneToMany(mappedBy="admin")
+	private List<ModelTetrimino>tetrimino;
 }
