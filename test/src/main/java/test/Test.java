15614e36fa09940ae031c4e923ffc81664792dc6
Unknown <quentin.darrigade@gmail.com>
2018-01-19 11:45:02 +0100

    Changement MappedBy

--- test/src/main/java/test/Test.java
+++ test/src/main/java/test/Test.java
@@ -5,25 +5,25 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import config.AppConfig;
-import dao.DaoTetrimino;
+
 import model.ModelTetrimino;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		AnnotationConfigApplicationContext myContext = new AnnotationConfigApplicationContext(AppConfig.class);		ModelTetrimino test = myContext.getBean(DaoTetrimino.class).getPieceById(1);
+		AnnotationConfigApplicationContext myContext = new AnnotationConfigApplicationContext(AppConfig.class);		
 		
 			
 	}
 	
-	public static String listerPiece(ApplicationContext myContext) {
-		String result = "";
-		for (ModelTetrimino mt1 : myContext.getBean(DaoTetrimino.class).getAllPiece()) {
-			result+=mt1.toString() + "\n";
-		}
-		
-		return result;
-	}
+//	public static String listerPiece(ApplicationContext myContext) {
+//		String result = "";
+//		for (ModelTetrimino mt1 : myContext.getBean(DaoTetrimino.class).getAllPiece()) {
+//			result+=mt1.toString() + "\n";
+//		}
+//		
+//		return result;
+//	}
 
 }
