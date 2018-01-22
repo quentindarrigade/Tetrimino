9f8cb463f359811f24585e96e1160a6ddcf03d2c
Unknown <quentin.darrigade@gmail.com>
2018-01-22 09:58:04 +0100

    Modifs

---
+++ dao/src/main/java/dao/IFAQDAO.java
@@ -0,0 +1,9 @@
+package dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.ModelAdmin;
+
+public interface IFAQDAO extends JpaRepository< ModelAdmin, Integer>{
+
+}
