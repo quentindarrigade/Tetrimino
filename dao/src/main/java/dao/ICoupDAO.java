ef8674a2ad25986fd4765877203dea21807a7e65
Unknown <tommybrias@hotmail.com>
2018-01-22 12:19:53 +0100

    Auto stash before merge of "dev" and "DevTommy"

--- dao/src/main/java/dao/ICoupDAO.java
+++ dao/src/main/java/dao/ICoupDAO.java
@@ -3,7 +3,8 @@ package dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import model.ModelAdmin;
+import model.ModelCoup;
 
-public interface ICoupDAO extends JpaRepository< ModelAdmin, Integer> {
+public interface ICoupDAO extends JpaRepository< ModelCoup, Integer> {
 
 }
